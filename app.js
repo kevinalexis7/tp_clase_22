f36e70835d4440cd505b074c53ccfe7e3d0b8bd3
Kevinalexis7 <kevinalex.espinola09@gmail.com>
2023-11-10 13:52:54 -0300

    Formulario OK

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require('path');
 const PORT = 3030;
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get ('/', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')))
+app.get ('/', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get ('/register', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'register.html')));
+app.get ('/login', (req,res)=>res.sendFile(path.join(__dirname, 'views', 'login.html')));
 
 app.listen(PORT, console.log(`Server running in http://localhost:${PORT}`))
